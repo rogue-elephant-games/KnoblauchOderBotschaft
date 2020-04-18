16231102f2c01c4017d781737e5b945a72591083
Rogue Elephant <jup.trandell@gmail.com>
2020-04-18 19:59:15 +0100

    Made game cycle images

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,19 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Gameplay : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI response;
+    [SerializeField] TextMeshProUGUI timeText;
+    public Image image; //change this image
+    public Sprite[] spritearray;
+    public float timeLeft = 20.0f;
+    int index = 0;
+
+    SceneLoader sceneLoader;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // image. = Resources.Load<TextAsset>("Text/textFile01");
+        sceneLoader = GetComponent<SceneLoader>();
+        GetNextPhoto();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeLeft >= 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeText.text = (timeLeft).ToString("0");
+        }
+        else
+            sceneLoader.LoadGameOverScene();
+
+    }
+
+    void GetNextPhoto()
+    {
+        index += 1;
+        if (index > spritearray.Length - 1)
+        {//if index is too big
+            index = 0; //loop back around
+        }
+        image.sprite = spritearray[index];
+    }
+
+    public void CheckAnswer(bool isKnobLauch)
+    {
+        var imageIsKnoblauch = image.sprite.name.ToLower().Contains("knoblauch");
+        if(
+            (isKnobLauch && imageIsKnoblauch)
+            || (!isKnobLauch && !imageIsKnoblauch)
+            )
+            GetNextPhoto();
+        else
+            sceneLoader.LoadGameOverScene();
+
     }
 }
